9e6a1c79e2420a6af94b81b44b2c9998125bca24
adnan2508 <adnandiu2800@gmail.com>
2024-12-01 09:43:08 +0600

    first commit

---
+++ data/todos.js
@@ -0,0 +1,52 @@
+export const data = [
+  {
+    "id": 1,
+    "title": "Finish reading the book",
+    "completed": true
+  },
+  {
+    "id": 2,
+    "title": "Buy groceries",
+    "completed": false
+  },
+  {
+    "id": 3,
+    "title": "Write a blog post",
+    "completed": true
+  },
+  {
+    "id": 4,
+    "title": "Clean the house",
+    "completed": false
+  },
+  {
+    "id": 5,
+    "title": "Work out",
+    "completed": false
+  },
+  {
+    "id": 6,
+    "title": "Call the bank",
+    "completed": false
+  },
+  {
+    "id": 7,
+    "title": "Fix the leaking faucet",
+    "completed": true
+  },
+  {
+    "id": 8,
+    "title": "Plan the trip itinerary",
+    "completed": false
+  },
+  {
+    "id": 9,
+    "title": "Attend the meeting",
+    "completed": true
+  },
+  {
+    "id": 10,
+    "title": "Finish the project report",
+    "completed": false
+  },
+]
